262c9a68855d667526872d1d68a8d25c00c23095
Johnathan Yang <johnnyhy@umich.edu>
2021-11-28 13:46:26 -0500

    appending cold starts to tracefile

--- processing.py
+++ processing.py
@@ -7,12 +7,15 @@ from csv import DictReader, DictWriter
 def main():
     filename = ""
     outfilename = filename + ".out"
+    outfile = open(outfilename, 'w')
+    writer = DictWriter(outfile)
     with open(filename, 'r') as csvfile:
         reader = DictReader(csvfile)
         for row in reader:
-            # get cold start time
-            # append cold start time to line
-            print("nothing")
+            # estimate and append cold starts to trace file
+            cold_start = cold_start_from_memory(row["language"], row["memory"])
+            row["cold_start_memory"] = cold_start
+            writer.writerow(row)
 
 
 if __name__ == "__main__":
